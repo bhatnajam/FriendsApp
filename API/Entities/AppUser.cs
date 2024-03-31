1e3a96aaefd738746329927c9f04df14a69f42ae
najambhat <najam.s@flotillatechs.com>
2024-03-31 15:42:02 +0530

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //auto implemented properties
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        // full properties
+        // private int myVar;
+        // public int MyProperty
+        // {
+        //     get { return myVar; }
+        //     set { myVar = value; }
+        // }
+        
+    }
+}
